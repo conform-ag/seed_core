beab29b087d20d2539125507c85051447689180b
Saad Ramadan <rmdn@users.noreply.github.com>
2026-01-09 16:36:01 +0100

    feat(ui): make segment optional and cascade filter by crop

--- seed_core/seed_core/doctype/seed_variety/seed_variety.js
+++ seed_core/seed_core/doctype/seed_variety/seed_variety.js
@@ -1,9 +1,25 @@
 frappe.ui.form.on('Seed Variety', {
+    setup: function (frm) {
+        frm.set_query('segment', function () {
+            return {
+                filters: {
+                    crop: frm.doc.crop
+                }
+            };
+        });
+    },
+
     refresh: function (frm) {
         if (!frm.is_new()) {
             frm.add_custom_button(__('Sync Item'), function () {
                 frm.call('sync_item');
             }, __('Actions'));
+        }
+    },
+
+    crop: function (frm) {
+        if (frm.doc.segment) {
+            frm.set_value('segment', '');
         }
     }
 });
